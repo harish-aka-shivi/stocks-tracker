ec18f0e43edc75ba2b41e1f222d0da39a4a92a0b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-11 12:14:59 +0000

    Deploy harish-aka-shivi/stocks-tracker to github.com/harish-aka-shivi/stocks-tracker.git:gh-pages

---
+++ precache-manifest.b2032af455b308d55cc4ccef8ede9397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a651c0389db09630e18b6c164e6beb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c5c8b771f34eb03fd3",
+    "url": "/static/css/main.c6244c62.chunk.css"
+  },
+  {
+    "revision": "06ef09d4a3ae3395f168",
+    "url": "/static/js/2.cca24464.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.cca24464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c5c8b771f34eb03fd3",
+    "url": "/static/js/main.87a125b7.chunk.js"
+  },
+  {
+    "revision": "ed28c2b7fa223d974f92",
+    "url": "/static/js/runtime-main.f226d3ab.js"
+  }
+]);
--- src/config.js
+++
@@ -1,1 +0,0 @@
-export const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKET_URL;
